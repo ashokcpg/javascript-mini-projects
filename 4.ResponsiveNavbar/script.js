8fb335fb2ed7d923e9076c5a1114762155ae9a8c
Ashok Chapagai <chapagaiashok@gmail.com>
2020-08-23 04:35:42 +0545

    Responsive NavBar

---
+++ 4.ResponsiveNavbar/script.js
@@ -0,0 +1,23 @@
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    // console.log(links.classList);
+    // console.log(links.classList.contains('random'));
+
+    // console.log(links.classList.contains('links'));
+
+    // Using one liner...
+
+    links.classList.toggle('show-links');
+
+    // Using if...else
+
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // }else{
+    //     links.classList.add('show-links');
+    // }
+});
